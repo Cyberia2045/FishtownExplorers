e28650f8c0f8a56c43d3c6fd69c868b4ba0bd828
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-24 14:37:04 -0400

    Added the ability to move map marker by address

--- FishtownExplorers/app/assets/javascripts/maps.js
+++ FishtownExplorers/app/assets/javascripts/maps.js
@@ -1,10 +1,10 @@
-  function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-} // concludes the initMap function
+//   function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 8
+//   });
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map
+//   });
+// } // concludes the initMap function
