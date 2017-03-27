f11d357947fa967bbfe8e0dcb5cd84e2f41207d8
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-26 22:39:14 -0400

    added routes and views for activity categories

---
+++ FishtownExplorers/app/assets/javascripts/places.coffee
@@ -0,0 +1,3 @@
+$(".libations").click(function() {
+	
+});
--- FishtownExplorers/config/routes.rb
+++ FishtownExplorers/config/routes.rb
@@ -5,7 +5,19 @@ Rails.application.routes.draw do
   root to: "home#index"
   get "/itinerary", to: "itineraries#show"
 
-  get "/categories", to: "home#categories"
-
   post "/locate", to: "geo#locate"
+
+  get "/categories", to: "places#categories"
+  get "/libations", to: "places#libations"
+  get "/pizza", to: "places#pizza"
+  get "/coffee", to: "places#coffee"
+  get "/parks", to: "places#parks"
+  get "/shops", to: "places#shops"
+  get "/fitness", to: "places#fitness"
+  get "/music", to: "places#music"
+  get "/restuarants", to: "places#restaurants"
+  get "/art", to: "places#art"
+  get "lodging", to: "places#lodging"
+  get "/allTheThings", to: "places#misc"
+  get "/transportation", to: "places#transit"
 end
